345bf314bae127bf6bfeff008292d453e13a60b9
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-21 19:00:57 +0000

    commit tree e4ab2bbd13b45f6fe23937fc629d3e345b6b04d3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  #  badges = []
+   # attendees.each do |name|
+    #badges << ("Hello, my name is #{name}.")
+    #end
+    #return badges
+    names.map { |name| badge_maker(name)}
+end
+
+def assign_rooms(room_assignments)
+    rooms = []
+    room_assignments.each.with_index(1) do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    return rooms
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|oreo| puts oreo}
+    assign_rooms(attendees).each{|candy| puts candy}
+end
+
+
